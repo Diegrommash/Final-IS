6e88559f77ffc489a668aa6828fb1c9edd159a72
Diegrommash <diegotereszczuk@gmail.com>
2025-07-26 09:58:42 -0300

    COMPO CON LOGIN

--- Final-IdS-Composite/UI/Program.cs
+++ Final-IdS-Composite/UI/Program.cs
@@ -1,3 +1,5 @@
+using BLL;
+
 namespace UI
 {
     internal static class Program
@@ -11,7 +13,8 @@ namespace UI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            var servicioLogin = new ServicioLogin();
+            frmLogin.Mostrar(() => new frmMainComposite(), servicioLogin);
         }
     }
 }
--- Final-IdS-Decorator/DAL/Acceso.cs
+++ Final-IdS-Decorator/DAL/Acceso.cs
@@ -13,7 +13,7 @@ namespace DAL
 
         public Acceso()
         {
-            _cadenaConexion = Configuracion.ObtenerCadenaConexion("EnTrabajo");
+            _cadenaConexion = Configuracion.ObtenerCadenaConexion("EnCasa");
 
             if (string.IsNullOrWhiteSpace(_cadenaConexion))
                 throw new InvalidOperationException("La cadena de conexión está vacía o no fue configurada.");
